6831ebba9ab75a4c8f515140663960615f2b2817
ismeh <gadeismael@hotmail.com>
2025-12-16 20:32:57 +0100

    :sparkles: feat: configure application properties and add security setup - h2 & h2-console working - add example schema.sql to populate data in H2 DB - create SecurityConfig and disable security - create application.yaml per enviroment dev & prod - add keycloack to pom

--- backend/src/main/java/com/tfm/tennis_platform/TennisPlatformApplication.java
+++ backend/src/main/java/com/tfm/tennis_platform/TennisPlatformApplication.java
@@ -8,11 +8,11 @@ import org.springframework.core.env.Environment;
 @SpringBootApplication
 public class TennisPlatformApplication {
 
-	public static void main(String[] args) {
+	 static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(TennisPlatformApplication.class, args);
         Environment env = context.getEnvironment();
         String port = env.getProperty("server.port", "8080"); // Default to 8080 if not set
-        System.out.println("Application started at: http://localhost:" + port + "------------------------------------------------");
+        System.out.println("Application started at: http://localhost:" + port + " ----------------------------------------------------------");
 
     }
 
