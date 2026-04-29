b405f6e991458a9903b44200536ee316905cac27
ismeh <gadeismael@hotmail.com>
2026-04-29 22:27:23 +0200

    refactor(infrastructure): Implement persistence adapters from domain ports

---
+++ backend/src/main/java/com/tfm/tennis_platform/infrastructure/persistence/adapter/AgeCategoryRefRepositoryAdapter.java
@@ -0,0 +1,23 @@
+package com.tfm.tennis_platform.infrastructure.persistence.adapter;
+
+import com.tfm.tennis_platform.domain.models.AgeCategoryRef;
+import com.tfm.tennis_platform.domain.port.out.AgeCategoryRefRepository;
+import com.tfm.tennis_platform.infrastructure.persistence.mapper.AgeCategoryRefMapper;
+import com.tfm.tennis_platform.infrastructure.persistence.repository.RefAgeCategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class AgeCategoryRefRepositoryAdapter implements AgeCategoryRefRepository {
+
+    private final RefAgeCategoryRepository repository;
+    private final AgeCategoryRefMapper mapper;
+
+    @Override
+    public List<AgeCategoryRef> findAll() {
+        return repository.findAll().stream().map(mapper::toDomain).toList();
+    }
+}
