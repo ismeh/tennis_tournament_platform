1ec8f134b95241720cfabf916bde5a42db32aa0b
ismeh <gadeismael@hotmail.com>
2026-06-22 21:28:08 +0200

    🗑️ refactor(roles): remove Role model and related repository components

--- backend/src/main/java/com/tfm/tennis_platform/domain/port/out/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tfm.tennis_platform.domain.port.out;
-
-import com.tfm.tennis_platform.domain.models.Role;
-import java.util.Optional;
-
-public interface RoleRepository {
-    Optional<Role> findByName(String name);
-}
